f33cb95d5c3e50320adba287d8221f730ff7e528
m020202 <m020202@naver.com>
2024-08-08 11:45:33 +0900

    first init

---
+++ src/main/java/jwt_v1/com/ComApplication.java
@@ -0,0 +1,13 @@
+package jwt_v1.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComApplication.class, args);
+	}
+
+}
